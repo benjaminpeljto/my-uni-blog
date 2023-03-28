9b0b4d15289838a8d5aec363ce22bd24ceeb434e
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-03-28 18:42:49 +0200

    flight get methods

--- rest/index.php
+++ rest/index.php
@@ -1,13 +1,16 @@
 <?php
     require '../vendor/autoload.php';
+    require 'dao/UsersDao.class.php';
 
 
-    Flight::route("/",function(){
-        echo "default route";
+    Flight::register("userDao","UsersDao");
+
+    Flight::route("GET /users",function(){
+        Flight::json(Flight::userDao()->get_all());  
     });
 
-    Flight::route("GET /users", function(){
-        echo "this is /users route";
+    Flight::route("GET /users/@id", function($id){
+        Flight::json(Flight::userDao()->get_by_id($id));
     });
 
     Flight::start();
