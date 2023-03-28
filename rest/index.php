603cc70746e5da3431950d3f779f047da9114722
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-03-28 21:40:10 +0200

    full route update in connection to the db and CRUD operations

--- rest/index.php
+++ rest/index.php
@@ -13,6 +13,23 @@
         Flight::json(Flight::userDao()->get_by_id($id));
     });
 
+    Flight::route("DELETE /users/@id", function($id){
+        Flight::userDao()->delete_user($id);
+        Flight::json(['message'=>'User by id ' . $id . ' has been deleted.']);
+    });
+
+    Flight::route("POST /users", function(){
+        $data = Flight::request()->data->getData();
+        $response = Flight::userDao()->add_user($data);
+        Flight::json(['message'=>'User added sucessfully.','Data: ' => $response]);
+        
+    });
+
+    Flight::route("PUT /users/@id", function($id){
+        $data = Flight::request()->data->getData();
+        $response = Flight::userDao()->update_user($data,$id);
+        Flight::json(['message'=>'Updated user with new data.','Data'=> $response]);
+    });
     Flight::start();
     
 ?>
