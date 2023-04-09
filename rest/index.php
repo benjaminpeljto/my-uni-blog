00c2ad5eb9c97ce6f63d0f924d4322ace96cdbc6
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-04-10 01:29:28 +0200

    tweeking in order to work with services and routes

--- rest/index.php
+++ rest/index.php
@@ -2,16 +2,17 @@
     require_once '../vendor/autoload.php';
 
     // import and register all business logic files (services) to FlightPHP
-    require_once 'services/UserService.php';
-    Flight::register("userService","UserService");
+    require_once __DIR__ . '/services/UserService.php';
+    
+    Flight::register('userService',"UserService");
 
     // import routes
-    require_once 'routes/UserRoutes.php';
+    require_once __DIR__ . '\routes\UserRoutes.php';
+
 
     //custom routes here
-
     Flight::route("GET /", function(){
-        echo "Welcome to the homepage";
+        echo "Start page";
     });
 
     Flight::start();
