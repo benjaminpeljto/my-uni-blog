957bf7e55053945f1f8ba5c9d3aad0a3c6559a99
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-04-10 01:28:46 +0200

    routes separated from index.php

--- rest/routes/UserRoutes.php
+++ rest/routes/UserRoutes.php
@@ -1,34 +1,31 @@
 <?php
-
-
-
     Flight::route("GET /users",function(){
-        Flight::json(Flight::userDao()->get_all());  
+        Flight::json(Flight::userService()->get_all());  
     });
 
     Flight::route("GET /users/@id", function($id){
-        Flight::json(Flight::userDao()->get_by_id($id));
+        Flight::json(Flight::userService()->get_by_id($id));
     });
 
     Flight::route("GET /usersById", function(){
-        Flight::json(Flight::userDao()->get_by_id(Flight::request()->query['id']));
+        Flight::json(Flight::userService()->get_by_id(Flight::request()->query['id']));
     });
 
     Flight::route("DELETE /users/@id", function($id){
-        Flight::userDao()->delete($id);
+        Flight::userService()->delete($id);
         Flight::json(['message'=>'User by id ' . $id . ' has been deleted.']);
     });
 
     Flight::route("POST /users", function(){
         $data = Flight::request()->data->getData();
-        $response = Flight::userDao()->add($data);
+        $response = Flight::userService()->add($data);
         Flight::json(['message'=>'User added sucessfully.','Data: ' => $response]);
         
     });
 
     Flight::route("PUT /users/@id", function($id){
         $data = Flight::request()->data->getData();
-        $response = Flight::userDao()->update($data,$id);
+        $response = Flight::userService()->update($data,$id);
         Flight::json(['message'=>'Updated user with new data.','Data'=> $response]);
     });
 
