f2e64c240714fdd7b31cb350296c4e95a0e87369
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-06-12 17:17:36 +0200

    trying to make it go live 3

--- rest/index.php
+++ rest/index.php
@@ -6,17 +6,14 @@ require_once __DIR__ . '/services/UserService.php';
 require_once __DIR__ . '/services/BlogService.php';
 require_once __DIR__ . '/services/CommentService.php';
 
-
-
 Flight::register('userService', "UserService");
 Flight::register('blogService', "BlogService");
 Flight::register('commentService', "CommentService");
 
-
 // import routes
-require_once __DIR__ . '\routes\UserRoutes.php';
-require_once __DIR__ . '\routes\BlogRoutes.php';
-require_once __DIR__ . '\routes\CommentRoutes.php';
+require_once __DIR__ . '/routes/UserRoutes.php';
+require_once __DIR__ . '/routes/BlogRoutes.php';
+require_once __DIR__ . '/routes/CommentRoutes.php';
 
 //custom routes here
 Flight::route("GET /", function () {
