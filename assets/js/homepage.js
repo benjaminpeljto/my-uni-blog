5f48fcd150baacfd79ba50db16868d06d9765302
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-06-15 20:01:27 +0200

    spapp with changing navbar

---
+++ assets/js/homepage.js
@@ -0,0 +1,4 @@
+var nav_login = "<li class=\"nav-item\"><a class=\"nav-link px-lg-3 py-3 py-lg-4\" href=\"#home\">Home</a></li>\n" +
+    "                    <li class=\"nav-item\"><a class=\"nav-link px-lg-3 py-3 py-lg-4\" href=\"#about\">About</a></li>\n" +
+    "                    <li class=\"nav-item\"><a class=\"nav-link px-lg-3 py-3 py-lg-4\" href=\"#contact\">Contact</a></li>"
+$("#tabs").html(nav_login)
