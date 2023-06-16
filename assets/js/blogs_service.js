db635188f9e0d98e7b1094128cbe8833c5b5e449
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-06-16 17:32:56 +0200

    get blogs ajax

--- assets/js/blogs_service.js
+++ assets/js/blogs_service.js
@@ -1,3 +1,42 @@
-$.get('/rest/blogswithuser', function (data){
-    console.log(data)
+$.get('rest/blogswithuser', function (data){
+    var blogsHtml = "";
+
+    for(var i = 0; i < data.length; i++){
+        var eachBlog = "";
+        eachBlog = " <!-- Post preview-->\n" +
+            "            <div class=\"post-preview\">\n" +
+            "                <a href=\"\">\n" +
+            "                    <h2 class=\"post-title\">" + data[i].title + "</h2>\n" +
+            "                    <h3 class=\"post-subtitle\">" + getFirstSentence(data[i].content) + "</h3>\n" +
+            "                </a>\n" +
+            "                <p class=\"post-meta\">\n" +
+            "                    Posted by\n" +
+            "                    <a href=\"\">" + data[i].user + "</a>\n" +
+            "                    on " + formatDate(data[i].create_time) + "\n" +
+            "                </p>\n" +
+            "            </div>\n" +
+            "            <!-- Divider-->\n" +
+            "            <hr class=\"my-4\" /> \n"
+
+        blogsHtml += eachBlog;
+    }
+
+    $("#blogs").html(blogsHtml);
 });
+
+
+function getFirstSentence(text) {
+    const firstSentence = text.match(/(.*?)([.?!])/);
+    if (firstSentence) {
+        return firstSentence[0];
+    }
+    return '';
+}
+
+function formatDate(dateString) {
+    const dateTimeParts = dateString.split(' ');
+    const datePart = dateTimeParts[0];
+    const dateParts = datePart.split('-');
+    const formattedDate = `${dateParts[2]}.${dateParts[1]}.${dateParts[0]}`;
+    return formattedDate;
+}
