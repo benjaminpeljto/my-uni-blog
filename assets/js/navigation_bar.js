59256c7efc1b7f69e9c4f772cdc0e85e1bb72c11
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-06-16 16:33:42 +0200

    new templates

---
+++ assets/js/navigation_bar.js
@@ -0,0 +1,14 @@
+var nav_login = "<li class=\"nav-item\"><a class=\"nav-link px-lg-3 py-3 py-lg-4\" href=\"#login\">Login</a></li>\n" +
+    "                    <li class=\"nav-item\"><a class=\"nav-link px-lg-3 py-3 py-lg-4\" href=\"#about\">About</a></li>\n" +
+    "                    <li class=\"nav-item\"><a class=\"nav-link px-lg-3 py-3 py-lg-4\" href=\"#contact\">Contact</a></li>"
+
+var nav_logged = "<li class=\"nav-item\"><a class=\"nav-link px-lg-3 py-3 py-lg-4\" href=\"\">Create</a></li>\n" +
+    "                    <li class=\"nav-item\"><a class=\"nav-link px-lg-3 py-3 py-lg-4\" href=\"#favorites\">Favorites</a></li>\n" +
+    "                    <li class=\"nav-item\"><a class=\"nav-link px-lg-3 py-3 py-lg-4\" href=\"#home\">Home</a></li>\n" +
+    "                    <li class=\"nav-item\"><a class=\"nav-link px-lg-3 py-3 py-lg-4\" href=\"#profile\">Profile</a></li>"
+$("#tabs").html(nav_logged)
+
+// Collapse navbar on link click
+$('.nav-link').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+});
