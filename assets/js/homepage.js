c96b5a054b77f06e8949472f09dacba37bbbd046
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-06-16 16:35:42 +0200

    from here to nav.js

--- assets/js/homepage.js
+++
@@ -1,9 +0,0 @@
-var nav_login = "<li class=\"nav-item\"><a class=\"nav-link px-lg-3 py-3 py-lg-4\" href=\"#home\">Home</a></li>\n" +
-    "                    <li class=\"nav-item\"><a class=\"nav-link px-lg-3 py-3 py-lg-4\" href=\"#about\">About</a></li>\n" +
-    "                    <li class=\"nav-item\"><a class=\"nav-link px-lg-3 py-3 py-lg-4\" href=\"#contact\">Contact</a></li>"
-$("#tabs").html(nav_login)
-
-// Collapse navbar on link click
-$('.nav-link').on('click', function () {
-    $('.navbar-collapse').collapse('hide');
-});
