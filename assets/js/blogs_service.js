1d5e26654a3e34cd3fc4593acafd322110532251
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-06-17 04:36:31 +0200

    ajax for blog details

--- assets/js/blogs_service.js
+++ assets/js/blogs_service.js
@@ -4,8 +4,8 @@ $.get('rest/blogswithuser', function (data){
     for(var i = 0; i < data.length; i++){
         var eachBlog = "";
         eachBlog = " <!-- Post preview-->\n" +
-            "            <div class=\"post-preview\">\n" +
-            "                <a href=\"\">\n" +
+            "            <div class=\"post-preview\" data-blog-id=\"" + data[i].id + "\">\n" +
+            "                <a class=\"blog-post\">\n" +
             "                    <h2 class=\"post-title\">" + data[i].title + "</h2>\n" +
             "                    <h3 class=\"post-subtitle\">" + getFirstSentence(data[i].content) + "</h3>\n" +
             "                </a>\n" +
@@ -22,7 +22,32 @@ $.get('rest/blogswithuser', function (data){
     }
 
     $("#blogs").html(blogsHtml);
+
 });
+
+
+
+
+$(document).on("click", ".post-preview", function() {
+    var blogId = $(this).data("blog-id");
+    console.log("Clicked blog ID:", blogId);
+    var currentUrl = window.location.href;
+    var urlParts = currentUrl.split("/");
+    urlParts[urlParts.length - 1] = "#blog";
+    var newUrl = urlParts.join("/");
+    console.log(newUrl);
+    window.location.href = newUrl;
+
+    $.get('rest/blogwithuser/' + blogId, function(data){
+        $(".blog-title").html(data[0].title);
+        $(".blog-subtitle").html(getFirstSentence(data[0].content));
+        $(".user").html(data[0].user);
+        $(".date").html(formatDate(data[0].create_time));
+        $("#blog-content").html(data[0].content);
+    });
+});
+
+
 
 
 function getFirstSentence(text) {
@@ -37,6 +62,9 @@ function formatDate(dateString) {
     const dateTimeParts = dateString.split(' ');
     const datePart = dateTimeParts[0];
     const dateParts = datePart.split('-');
-    const formattedDate = `${dateParts[2]}.${dateParts[1]}.${dateParts[0]}`;
+    const year = dateParts[0];
+    const month = new Date(dateString).toLocaleString('default', { month: 'long' });
+    const day = dateParts[2];
+    const formattedDate = `${month} ${day}, ${year}`;
     return formattedDate;
 }
