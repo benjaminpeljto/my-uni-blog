935f2017c673012e674cc554c77b40ae80352711
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-06-17 23:55:53 +0200

    blog id hidden

--- assets/js/blogs_service.js
+++ assets/js/blogs_service.js
@@ -1,49 +1,85 @@
-$.get('rest/blogswithuser', function(data) {
-    var blogsHtml = "";
 
-    for (var i = 0; i < data.length; i++) {
-        var eachBlog = "";
-        eachBlog = `
-            <!-- Post preview -->
-            <div class="post-preview">
-                <a class="blog-post" data-blog-id="${data[i].id}">
-                    <h2 class="post-title">${data[i].title}</h2>
-                    <h3 class="post-subtitle">${getFirstSentence(data[i].content)}</h3>
-                </a>
-                <p class="post-meta">
-                    Posted by
-                    <a href="">${data[i].user}</a>
-                    on ${formatDate(data[i].create_time)}
-                </p>
-            </div>
-            <!-- Divider -->
-            <hr class="my-4" />
-        `;
+$(function() {
 
-        blogsHtml += eachBlog;
-    }
+    $.get('rest/blogswithuser', function(data) {
+        var blogsHtml = "";
 
-    $("#blogs").html(blogsHtml);
+        for (var i = 0; i < data.length; i++) {
+            var eachBlog = "";
+            eachBlog = `
+                <!-- Post preview -->
+                <div class="post-preview">
+                    <a class="blog-post" onclick="openBlogDetails(${data[i].id})">
+                        <h2 class="post-title">${data[i].title}</h2>
+                        <h3 class="post-subtitle">${getFirstSentence(data[i].content)}</h3>
+                        <input name="postId" value="${data[i].id}" hidden>
+                    </a>
+                    <p class="post-meta">
+                        Posted by
+                        <a href="">${data[i].user}</a>
+                        on ${formatDate(data[i].create_time)}
+                    </p>
+                </div>
+                <!-- Divider -->
+                <hr class="my-4" />
+            `;
+
+            blogsHtml += eachBlog;
+        }
+
+        $("#blogs").html(blogsHtml);
+    });
+
+    // $(document).on("click", ".blog-post", function() {
+    //     var blogId = $(this).data("blog-id");
+    //     console.log("Clicked blog ID:", blogId);
+    //     var currentUrl = window.location.href;
+    //     var urlParts = currentUrl.split("/");
+    //     urlParts[urlParts.length - 1] = "#blog";
+    //     var newUrl = urlParts.join("/");
+    //     console.log(newUrl);
+    //     window.location.href = newUrl;
+    //
+    //     $.get('rest/blogwithuser/' + blogId, function(data) {
+    //         $(".blog-title").html(data[0].title);
+    //         $(".blog-subtitle").html(getFirstSentence(data[0].content));
+    //         $(".user").html(data[0].user);
+    //         $(".date").html(formatDate(data[0].create_time));
+    //         $("#blog-content").html(data[0].content);
+    //     });
+    // });
+
+
+
+
 });
 
-$(document).on("click", ".blog-post", function() {
-    var blogId = $(this).data("blog-id");
-    console.log("Clicked blog ID:", blogId);
+
+function openBlogDetails(id){
     var currentUrl = window.location.href;
     var urlParts = currentUrl.split("/");
     urlParts[urlParts.length - 1] = "#blog";
     var newUrl = urlParts.join("/");
-    console.log(newUrl);
     window.location.href = newUrl;
 
-    $.get('rest/blogwithuser/' + blogId, function(data) {
+    $.get('rest/blogwithuser/' + id, function(data) {
         $(".blog-title").html(data[0].title);
         $(".blog-subtitle").html(getFirstSentence(data[0].content));
         $(".user").html(data[0].user);
         $(".date").html(formatDate(data[0].create_time));
         $("#blog-content").html(data[0].content);
     });
-});
+}
+
+function showCreateModal(){
+    $("#createBlogModal").modal("show");
+}
+function hideCreateModal(){
+    $("#createBlogModal").modal("hide");
+}
+
+
+
 
 function getFirstSentence(text) {
     const firstSentence = text.match(/(.*?)([.?!])/);
