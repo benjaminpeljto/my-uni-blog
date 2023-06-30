02b56bbe10788605e30a85efd04a0b5b952321f3
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-06-30 20:29:09 +0200

    utilities for jwt

---
+++ assets/js/utils.js
@@ -0,0 +1,30 @@
+var Utils = {
+
+    form2json: function(form){
+        var array = $(form).serializeArray();
+        var json = {};
+        $.each(array, function() {
+            json[this.name] = this.value || '';
+        });
+        return json;
+    },
+
+    parseJwt: function(token){
+        if (token) {
+            var base64Url = token.split(".")[1];
+            var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+            var jsonPayload = decodeURIComponent(
+                atob(base64)
+                    .split("")
+                    .map(function (c) {
+                        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+                    })
+                    .join("")
+            );
+            return JSON.parse(jsonPayload);
+        } else {
+            return null;
+        }
+    }
+
+}
