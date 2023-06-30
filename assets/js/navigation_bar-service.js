7628b35fcb00456f8bab5a64aab27ffdbcd1eb04
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-06-30 20:57:44 +0200

    corrections regarding blogs-service.js

--- assets/js/navigation_bar-service.js
+++ assets/js/navigation_bar-service.js
@@ -1,4 +1,4 @@
-const nav_logged = "<li class=\"nav-item\"><a class=\"createbuttonnavlink nav-link px-lg-3 py-3 py-lg-4\" onclick=\"showCreateModal()\">Create</a></li>\n" +
+const nav_logged = "<li class=\"nav-item\"><a class=\"createbuttonnavlink nav-link px-lg-3 py-3 py-lg-4\" onclick=\"BlogsService.openCreateModal()\">Create</a></li>\n" +
     "                    <li class=\"nav-item\"><a class=\"nav-link px-lg-3 py-3 py-lg-4\" href=\"#favorites\">Favorites</a></li>\n" +
     "                    <li class=\"nav-item\"><a class=\"nav-link px-lg-3 py-3 py-lg-4\" href=\"#home\">Home</a></li>\n" +
     "                    <li class=\"nav-item\"><a class=\"nav-link px-lg-3 py-3 py-lg-4\" href=\"#profile\">Profile</a></li>\n" +
