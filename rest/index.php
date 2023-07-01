2de4c342d5de3fe9a7aa2dd7988041a5bac294b4
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-07-01 17:15:27 +0200

    included files for favorites

--- rest/index.php
+++ rest/index.php
@@ -5,15 +5,18 @@ require_once '../vendor/autoload.php';
 require_once __DIR__ . '/services/UserService.php';
 require_once __DIR__ . '/services/BlogService.php';
 require_once __DIR__ . '/services/CommentService.php';
+require_once __DIR__ . '/services/FavoriteService.php';
 
 Flight::register('userService', "UserService");
 Flight::register('blogService', "BlogService");
 Flight::register('commentService', "CommentService");
+Flight::register('favoriteService', "FavoriteService");
 
 // import routes
 require_once __DIR__ . '/routes/UserRoutes.php';
 require_once __DIR__ . '/routes/BlogRoutes.php';
 require_once __DIR__ . '/routes/CommentRoutes.php';
+require_once __DIR__ . '/routes/FavoriteBlogRoutes.php';
 
 //custom routes here
 Flight::route("GET /", function () {
