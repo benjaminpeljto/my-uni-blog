291a6ce4b9c2c137cf43a03e3316cf959f685148
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-07-02 21:47:51 +0200

    added category dependencies

--- rest/index.php
+++ rest/index.php
@@ -6,17 +6,20 @@ require_once __DIR__ . '/services/UserService.php';
 require_once __DIR__ . '/services/BlogService.php';
 require_once __DIR__ . '/services/CommentService.php';
 require_once __DIR__ . '/services/FavoriteService.php';
+require_once __DIR__ . '/services/CategoryService.php';
 
 Flight::register('userService', "UserService");
 Flight::register('blogService', "BlogService");
 Flight::register('commentService', "CommentService");
 Flight::register('favoriteService', "FavoriteService");
+Flight::register('categoryService', "CategoryService");
 
 // import routes
 require_once __DIR__ . '/routes/UserRoutes.php';
 require_once __DIR__ . '/routes/BlogRoutes.php';
 require_once __DIR__ . '/routes/CommentRoutes.php';
 require_once __DIR__ . '/routes/FavoriteBlogRoutes.php';
+require_once __DIR__ . '/routes/CategoryRoutes.php';
 
 //custom routes here
 Flight::route("GET /", function () {
