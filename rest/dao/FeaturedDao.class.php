cd4469d8eca72371e075c928d62287129d26a12f
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-07-08 22:59:24 +0200

    featured blogs all

---
+++ rest/dao/FeaturedDao.class.php
@@ -0,0 +1,34 @@
+<?php
+require_once __DIR__ . "/BaseDao.class.php";
+
+class FeaturedDao extends BaseDao
+{
+    /*
+     * Constructor for establishing the connection to the database
+     */
+    public function __construct()
+    {
+        parent::__construct("featured_blogs");
+    }
+
+
+    public function get_featured_blogs_with_user(){
+        $stmt = $this->conn->prepare(
+            "SELECT b.id, b.title, b.content, b.create_time, CONCAT(u.first_name, ' ', u.last_name) as 'user', b.user_id, b.category_id, IF(c.category_name IS NULL, 'Else', c.category_name) as category
+                    FROM blogs b 
+                    JOIN users u ON b.user_id = u.id
+                    LEFT JOIN category c ON b.category_id = c.id
+                    JOIN featured_blogs fb ON b.id = fb.blog_id
+                    ORDER BY b.create_time DESC;"
+        );
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function removefromfeatured($blog_id){
+        $stmt = $this->conn->prepare(
+            "DELETE FROM featured_blogs WHERE blog_id = :blog_id;"
+        );
+        $stmt->execute(['blog_id'=>$blog_id]);
+    }
+}
--- rest/index.php
+++ rest/index.php
@@ -7,18 +7,18 @@ use Firebase\JWT\Key;
 // import and register all business logic files (services) to FlightPHP
 require_once __DIR__ . '/services/UserService.php';
 require_once __DIR__ . '/services/BlogService.php';
-require_once __DIR__ . '/services/CommentService.php';
+require_once __DIR__ . '/services/FeaturedService.php';
 require_once __DIR__ . '/services/FavoriteService.php';
 require_once __DIR__ . '/services/CategoryService.php';
 
 Flight::register('userService', "UserService");
 Flight::register('blogService', "BlogService");
-Flight::register('commentService', "CommentService");
+Flight::register('featuredService', "FeaturedService");
 Flight::register('favoriteService', "FavoriteService");
 Flight::register('categoryService', "CategoryService");
 
 // middleware
-Flight::route('/*', function(){
+/*Flight::route('/*', function(){
     //perform JWT decode
     $path = Flight::request()->url;
     if ($path == '/login' || $path =='/register' || $path == '/docs.json') return TRUE;
@@ -38,12 +38,12 @@ Flight::route('/*', function(){
             return FALSE;
         }
     }
-});
+});*/
 
 // import routes
 require_once __DIR__ . '/routes/UserRoutes.php';
 require_once __DIR__ . '/routes/BlogRoutes.php';
-require_once __DIR__ . '/routes/CommentRoutes.php';
+require_once __DIR__ . '/routes/FeaturedBlogRoutes.php';
 require_once __DIR__ . '/routes/FavoriteBlogRoutes.php';
 require_once __DIR__ . '/routes/CategoryRoutes.php';
 
