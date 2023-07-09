2c25bdd7e8acf17658ea17755895ba7b7ee34d8b
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-07-09 16:30:20 +0200

    swagger documentation for category routes

--- rest/routes/CategoryRoutes.php
+++ rest/routes/CategoryRoutes.php
@@ -1,4 +1,12 @@
 <?php
+
+/**
+ * @OA\Get(path="/category/{id}", tags={"Categories"}, security={{"ApiKeyAuth": {}}},
+ *     summary="Return blogs by category ID",
+ *     @OA\Parameter(in="path", name="id", example=4, description="Category ID", required=true),
+ *     @OA\Response(response="200", description="Get all blogs by category")
+ * )
+ */
     Flight::route("GET /category/@id", function ($id){
         Flight::json(Flight::categoryService()->get_blogs_by_category($id));
     });
