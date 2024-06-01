ffcbd727752987785fdc25f8d19b496edf6e32ed
Tarik-7 <82581093+Tarik-7@users.noreply.github.com>
2024-06-01 14:18:15 +0200

    Modified UserService class. Added new "get_profile_info" method which takes userID as an argument and returns user entity from database without admin and password attribute. Added method for updating profile image which handles the deletion of already exisiting image (if present) and adding url from imgur api to users table.

--- rest/services/UserService.php
+++ rest/services/UserService.php
@@ -23,6 +23,32 @@ class UserService extends BaseService{
         return $this->dao->get_number_of_users();
     }
 
+    function get_profile_info($user_id){
+        $array = $this->dao->get_by_id($user_id);
+        $user = $array[0];
+        unset($user['password']);
+        unset($user['admin']);
+        return $user;
+    }
+
+    function update_profile_picture($user_id, $image){
+        $user = $this->dao->get_by_id($user_id)[0];
+        if(!isset($user['id'])){
+            Flight::json(["message" => "User doesn't exist"], 404);
+        }
+        else{
+            $old_picture_delete_hash = $user['profile_picture_delete_hash'];
+            Flight::imgurService()->deleteImageAsync($old_picture_delete_hash);
+            $upload_data = Flight::imgurService()->uploadImageAsync($image['tmp_name']);
+            $result = $this->dao->update_profile_image($user_id, $upload_data[0], $upload_data[1]);
+            if($result){
+                Flight::json(['success'=>true, 'message'=>'You successfully changed your profile image.', 'image_url'=>$upload_data[0]], 200);
+            }
+            else{
+                Flight::json(['success'=>false, 'message'=>"Error occurred, please try again."], 500);
+            }
+        }
+    }
+
     
 }
-?>
