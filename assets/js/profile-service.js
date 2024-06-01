06640c790ff0dccb78a684f8d9681dc9f0ba50e3
Tarik-7 <82581093+Tarik-7@users.noreply.github.com>
2024-06-01 14:20:14 +0200

    Modified profile-service.js file. Added functionality for updating profile image.

--- assets/js/profile-service.js
+++ assets/js/profile-service.js
@@ -1,21 +1,73 @@
 var ProfileService = {
     init: function (){
         ProfileService.getProfileData();
+        ProfileService.changeProfileImage();
     },
 
     getProfileData: function (){
         RestClient.get(
-            "rest/users/" + Utils.getCurrentUserId(),
+            "rest/profile/" + Utils.getCurrentUserId(),
             function (data){
-                ProfileService.updateProfileData(data);
+                ProfileService.setProfileData(data);
             }
         )
     },
 
-    updateProfileData: function (data){
-        $("#profile-first-name").html(data[0].first_name);
-        $("#profile-last-name").html(data[0].last_name);
-        $("#profile-age").html(data[0].age);
-        $("#profile-email").html(data[0].email);
+    setProfileData: function (data){
+        $("#profile-first-name").html(data.first_name);
+        $("#profile-last-name").html(data.last_name);
+        $("#profile-age").html(data.age);
+        $("#profile-email").html(data.email);
+        $("#profile-profile-image").attr("src", data.profile_picture)
+    },
+
+    openImageChangeModal: function (){
+        $("#uploadImageModal").modal("show");
+    },
+
+    closeImageChangeModal: function (){
+        $("#uploadImageModal").modal("hide");
+    },
+
+    changeProfileImage: function(){
+        $("#profilePictureChangeForm").validate({
+            rules: {
+                image_file: {
+                    required: true
+                }
+            },
+            submitHandler: function(form, event){
+                event.preventDefault();
+                var formData = new FormData(form);
+                ProfileService.uploadNewProfilePicture(formData);
+            }
+        })
+    },
+
+    uploadNewProfilePicture: function (formData) {
+        $.ajax({
+            url: 'rest/profile/image-change/' + Utils.getCurrentUserId(), // Change this to your API endpoint
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            beforeSend: function(xhr){
+                if (localStorage.getItem("user_token")){
+                    xhr.setRequestHeader('Authentication', localStorage.getItem("user_token"));
+                }
+            },
+            success: function (response) {
+                $("#profile-profile-image").attr("src", response.image_url);
+
+                // Close the modal
+                ProfileService.closeImageChangeModal();
+                toastr.success(response.message)
+            },
+            error: function (xhr, status, error) {
+                toastr.error(error);
+            }
+        });
     }
+
+
 }
