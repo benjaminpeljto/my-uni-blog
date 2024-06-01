a7822aba2aa921437d712c39bce497b8e14c27ae
benjamin.peljto <benjaminpeljto00@gmail.com>
2024-06-01 12:58:04 +0200

    Added additional dao method which queries active blogs based on the user accessing the application. It additionally queries the database table likes and returns, in addition to blog information, an attribute if the user has liked the blog.

--- rest/dao/BlogsDao.class.php
+++ rest/dao/BlogsDao.class.php
@@ -14,13 +14,27 @@ class BlogsDao extends BaseDao
     public function get_blogs_with_user()
     {
         $stmt = $this->conn->prepare(
-            "SELECT b.id, b.title, b.content, b.create_time, CONCAT(u.first_name, ' ', u.last_name) AS 'user', b.user_id, b.category_id, IF(c.category_name IS NULL, 'Else', c.category_name) AS category
+            "SELECT b.id, b.title, b.content, b.create_time, CONCAT(u.first_name, ' ', u.last_name) AS 'user', u.profile_picture, b.user_id, b.category_id, IF(c.category_name IS NULL, 'Else', c.category_name) AS category
                     FROM blogs b 
                     JOIN users u ON b.user_id = u.id
                     LEFT JOIN category c ON b.category_id = c.id
                     ORDER BY b.create_time DESC;"
         );
         $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function get_blogs_with_user_for_user($user_id)
+    {
+        $stmt = $this->conn->prepare(
+            "SELECT b.id, b.title, b.content, b.create_time, CONCAT(u.first_name, ' ', u.last_name) AS 'user', u.profile_picture, b.user_id, b.category_id, IF(c.category_name IS NULL, 'Else', c.category_name) AS category, IF(l.user_id = :user_id, true, false) as liked_by_user
+                    FROM blogs b 
+                    JOIN users u ON b.user_id = u.id
+                    LEFT JOIN category c ON b.category_id = c.id
+                    LEFT JOIN likes l ON b.id = l.blog_id
+                    ORDER BY b.create_time DESC;"
+        );
+        $stmt->execute(['user_id'=>$user_id]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
