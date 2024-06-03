01c2688ba1dc9e3a5dd0a7bbd7b3ee9befd92bc6
benjamin.peljto <benjaminpeljto00@gmail.com>
2024-06-03 19:10:10 +0200

    Modified UserService.php Added business logic for updating profile info, for password change, for getting users for admin and ban/unban option for admin.

--- rest/services/UserService.php
+++ rest/services/UserService.php
@@ -50,5 +50,88 @@ class UserService extends BaseService{
         }
     }
 
-    
+    function updateProfileData($user_data, $user_id){
+        $user = $this->dao->get_by_id($user_id)[0];
+        if(isset($user['id'])){
+            if($user['password'] == md5($user_data['password'])){
+                unset($user_data['password']);
+                $returned_entity = $this->dao->update($user_data, $user_id);
+                if(isset($returned_entity['id'])){
+                    Flight::json(['success'=>true, 'message'=>"Profile data updated successfully."]);
+                } else {
+                    Flight::json(['success'=>false, "message"=>"Error updating profile data. Please contact developers."]);
+                }
+            } else {
+                Flight::json(['success'=>false, 'message'=>"Incorrect password"]);
+            }
+        } else {
+            Flight::json(['success'=>false, 'message'=>"User doesn't exist."]);
+        }
+    }
+
+    function changePassword($password_data, $user_id)
+    {
+        if ($password_data['password'] !== $password_data['password2']) {
+            Flight::json(['success' => false, 'message' => "Passwords don't match."]);
+        }
+
+        $user = $this->dao->get_by_id($user_id)[0];
+        if (isset($user['id'])) {
+            if ($user['password'] === md5($password_data['profileCurrentPassword'])) {
+                if($user['password'] === md5($password_data['password'])){
+                    Flight::json(['success'=>false, "message"=>"You cannot enter the same password."]);
+                } else {
+                    unset($password_data['password2']);
+                    unset($password_data['profileCurrentPassword']);
+                    $password_data['password'] = md5($password_data['password']);
+                    $returned_entity = $this->dao->update($password_data, $user_id);
+                    if(isset($returned_entity['id'])){
+                        Flight::json(['success'=>true, 'message'=>"Changed profile password."]);
+                    } else {
+                        Flight::json(['success'=>false, "message"=>"Error while changing password. Please contact developers."]);
+                    }
+                }
+            } else {
+                Flight::json(['success' => false, 'message' => "Incorrect password."]);
+            }
+        } else {
+            Flight::json(['success' => false, 'message' => "User doesn't exist."]);
+        }
+    }
+
+    function get_users_for_admin(){
+        Flight::json($this->dao->get_users_for_admin());
+    }
+
+    function ban_user($user_id){
+        $user = $this->dao->get_by_id($user_id)[0];
+        if(isset($user['id'])){
+            if($user['admin'] == 1){
+                Flight::json(['success'=>false, "message"=>"Something went wrong."]);
+            } else {
+                $rows_affected = $this->dao->ban_user($user_id);
+                if($rows_affected !== 1){
+                    Flight::json(['success'=>false, "message"=>"Something went wrong."]);
+                } else {
+                    Flight::json(['success'=>true, "message"=>"User with ID " . $user_id . " successfully banned."]);
+                }
+            }
+        } else{
+            Flight::json(['success'=>false, "message"=>"User doesn't exist."]);
+        }
+    }
+
+    function unban_user($user_id){
+        $user = $this->dao->get_by_id($user_id)[0];
+        if(isset($user['id'])){
+            $rows_affected = $this->dao->unban_user($user_id);
+            if($rows_affected !== 1){
+                Flight::json(['success'=>false, "message"=>"Something went wrong."]);
+            } else {
+                Flight::json(['success'=>true, "message"=>"User with ID " . $user_id . " successfully unbanned."]);
+            }
+        } else{
+            Flight::json(['success'=>false, "message"=>"User doesn't exist."]);
+        }
+    }
 }
