7dd655aea0362bcafb233780f667cac8fb4e9b6c
benjamin.peljto <benjaminpeljto00@gmail.com>
2024-06-03 20:05:44 +0200

    Added new database dump for ones who want to have a database for start and removed the first one.

--- dump-myuniblog-db-202406032004.sql
+++ dump-myuniblog-db-202406032004.sql
@@ -37,7 +37,7 @@ CREATE TABLE `blogs` (
   KEY `category_id_idx` (`category_id`),
   CONSTRAINT `category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -46,7 +46,7 @@ CREATE TABLE `blogs` (
 
 LOCK TABLES `blogs` WRITE;
 /*!40000 ALTER TABLE `blogs` DISABLE KEYS */;
-INSERT INTO `blogs` VALUES (26,'hehbhbds','kansjdna ajs da sdas dkas djas djas dja sjdh asjdjas djas djas dja sdj asjd asj dasn djas djasn djasn djas jas djasn dasn djasn djans djasn djasn djasn djnas djna sjdn asjdn ajsn dajs djasn djasn da. ajs bdias da. iasjd asd.','2024-05-31 00:12:44',0,41,3,NULL),(27,'Moj tajtl','ja kreno i hopala. eto tako smo mi. Kad god isli. krenuli i isli. ma sta god radili smo teku ponijeli i isli dugim stazama puta. aaaaaaaaa','2024-05-31 18:36:37',0,42,6,NULL),(28,'sdfsdfsd','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget ultricies nisl. Ut blandit tortor ac volutpat elementum. Mauris et bibendum ante. Curabitur tristique diam vel efficitur dignissim. In maximus egestas tincidunt. Nullam id mattis sem. Proin ac sodales ligula. Praesent ut scelerisque libero. Proin gravida dui sapien, non sollicitudin nulla imperdiet id. Quisque ut leo dui. Nam varius, nisl in bibendum aliquam, ligula odio posuere sem, non feugiat tellus velit at ante. Sed aliquam enim sit amet leo ullamcorper, vel rutrum sem venenatis. Vivamus pellentesque lorem felis, non finibus diam tempor vehicula. Vivamus eget magna non lacus volutpat efficitur id id lectus. Etiam bibendum interdum ex, eget ultricies neque.','2024-05-31 19:15:40',0,42,4,NULL),(29,'Lorem','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget ultricies nisl. Ut blandit tortor ac volutpat elementum. Mauris et bibendum ante. Curabitur tristique diam vel efficitur dignissim. In maximus egestas tincidunt. Nullam id mattis sem. Proin ac sodales ligula. Praesent ut scelerisque libero. Proin gravida dui sapien, non sollicitudin nulla imperdiet id. Quisque ut leo dui. Nam varius, nisl in bibendum aliquam, ligula odio posuere sem, non feugiat tellus velit at ante. Sed aliquam enim sit amet leo ullamcorper, vel rutrum sem venenatis. Vivamus pellentesque lorem felis, non finibus diam tempor vehicula. Vivamus eget magna non lacus volutpat efficitur id id lectus. Etiam bibendum interdum ex, eget ultricies neque.','2024-05-31 19:16:01',0,42,4,NULL);
+INSERT INTO `blogs` VALUES (60,'Lorem Ipsum','Nunc condimentum felis eu mollis ullamcorper. Duis eu ipsum lectus. Phasellus in orci consectetur, pulvinar tortor quis, feugiat dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent placerat mi mi, sit amet pulvinar massa accumsan vel. Pellentesque placerat quam in aliquet sodales. Praesent at lacus vitae tellus laoreet dictum. Cras cursus dui et diam finibus, eu malesuada ligula luctus. Vestibulum fermentum vitae arcu sed ornare. Fusce tellus nunc, convallis nec ante eget, elementum mollis nibh. Praesent tempus in sapien vitae maximus. Etiam auctor non libero eu fermentum. Vestibulum sit amet mi vel sem placerat imperdiet id id justo. Cras euismod euismod est. In molestie lectus sed dolor aliquet, id egestas lorem bibendum.','2024-06-03 19:55:17',0,42,5,NULL),(61,'Lorem opet','Mauris fermentum nulla sed nisl tristique, lobortis dapibus lorem feugiat. Aenean a dignissim sem, sed ornare justo. Sed in mattis velit, nec maximus ante. Mauris odio elit, dignissim semper tortor at, sollicitudin sodales dolor. Pellentesque in finibus nulla, vitae vehicula lorem. Nam lobortis semper ante sed iaculis. Duis malesuada eget lectus ut pretium. Morbi vitae ligula a metus rhoncus pretium in eu arcu. Vestibulum iaculis, tortor non laoreet fermentum, mi ex vestibulum metus, a luctus eros nibh nec dui. Pellentesque suscipit, quam sed lobortis semper, ante lacus ultrices velit, quis malesuada leo turpis aliquam urna.','2024-06-03 19:55:43',0,42,7,NULL),(62,'Idemo opet','Fusce vitae risus faucibus purus varius commodo gravida nec sapien. Quisque placerat imperdiet nulla ac placerat. Morbi ut ipsum sodales justo convallis viverra sed sit amet erat. Suspendisse potenti. Duis sagittis malesuada facilisis. Curabitur dolor ipsum, feugiat nec vestibulum et, tempor sit amet eros. Nam eu pulvinar nibh. Nam hendrerit sagittis orci in dapibus. Praesent condimentum nisi non dolor ultrices porta. Nulla pulvinar turpis non diam placerat, eget placerat neque dictum. Sed venenatis velit at ligula aliquam, ut lacinia mauris dapibus. Proin ac ligula et dolor fermentum fermentum. Donec sed elementum arcu. Pellentesque eget eleifend ex, eu fermentum elit. Vivamus dapibus mi ac lectus mattis, sed elementum nibh placerat.\r\n\r\n','2024-06-03 19:57:07',0,45,6,NULL),(63,'Nesto novo','Phasellus faucibus a dolor ac lobortis. Sed posuere dolor quis odio vulputate sodales. Suspendisse commodo justo libero, ullamcorper tempus magna porta ut. Fusce bibendum eleifend est. Proin semper varius risus sit amet euismod. Etiam vehicula sodales faucibus. Pellentesque ut lorem eros. Nam ut lectus commodo, iaculis diam vitae, consequat est. Aliquam hendrerit erat eget lacus rhoncus blandit. Vestibulum orci augue, porttitor in eleifend eu, posuere eu nisi. Pellentesque neque velit, vestibulum at justo id, sollicitudin convallis velit. Maecenas luctus massa eget lorem molestie sollicitudin id eleifend eros.','2024-06-03 19:59:26',0,40,5,NULL),(64,'Nesto novo','\"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...\"\r\nPhasellus faucibus a dolor ac lobortis. Sed posuere dolor quis odio vulputate sodales. Suspendisse commodo justo libero, ullamcorper tempus magna porta ut. Fusce bibendum eleifend est. Proin semper varius risus sit amet euismod. Etiam vehicula sodales faucibus. Pellentesque ut lorem eros. Nam ut lectus commodo, iaculis diam vitae, consequat est. Aliquam hendrerit erat eget lacus rhoncus blandit. Vestibulum orci augue, porttitor in eleifend eu, posuere eu nisi. Pellentesque neque velit, vestibulum at justo id, sollicitudin convallis velit. Maecenas luctus massa eget lorem molestie sollicitudin id eleifend eros.\r\n\r\n','2024-06-03 20:00:29',0,41,4,NULL),(65,'Eto zato','Odavno je uspostavljena činjenica da čitača ometa razumljivi tekst dok gleda raspored elemenata na stranici. Smisao korištenja Lorem Ipsum-a jest u tome što umjesto \'sadržaj ovjde, sadržaj ovjde\' imamo normalni raspored slova i riječi, pa čitač ima dojam da gleda tekst na razumljivom jeziku. Mnogi programi za stolno izdavaštvo i uređivanje web stranica danas koriste Lorem Ipsum kao zadani model teksta, i ako potražite \'lorem ipsum\' na Internetu, kao rezultat dobit ćete mnoge stranice u izradi. Razne verzije razvile su se tijekom svih tih godina, ponekad slučajno, ponekad namjerno (s dodatkom humora i slično).','2024-06-03 20:01:53',0,40,2,NULL);
 /*!40000 ALTER TABLE `blogs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -93,7 +93,7 @@ CREATE TABLE `favorite_blogs` (
   KEY `fb_blog_id_idx` (`blog_id`),
   CONSTRAINT `fb_blog_id` FOREIGN KEY (`blog_id`) REFERENCES `blogs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fb_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -102,7 +102,6 @@ CREATE TABLE `favorite_blogs` (
 
 LOCK TABLES `favorite_blogs` WRITE;
 /*!40000 ALTER TABLE `favorite_blogs` DISABLE KEYS */;
-INSERT INTO `favorite_blogs` VALUES (26,26,41);
 /*!40000 ALTER TABLE `favorite_blogs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -129,7 +128,6 @@ CREATE TABLE `featured_blogs` (
 
 LOCK TABLES `featured_blogs` WRITE;
 /*!40000 ALTER TABLE `featured_blogs` DISABLE KEYS */;
-INSERT INTO `featured_blogs` VALUES (19,26);
 /*!40000 ALTER TABLE `featured_blogs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -149,7 +147,7 @@ CREATE TABLE `likes` (
   KEY `likes_blogs_FK` (`blog_id`),
   CONSTRAINT `likes_blogs_FK` FOREIGN KEY (`blog_id`) REFERENCES `blogs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `likes_users_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=83 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -158,7 +156,7 @@ CREATE TABLE `likes` (
 
 LOCK TABLES `likes` WRITE;
 /*!40000 ALTER TABLE `likes` DISABLE KEYS */;
-INSERT INTO `likes` VALUES (31,42,29),(32,42,28);
+INSERT INTO `likes` VALUES (74,40,64),(75,40,62),(76,40,60),(77,41,64),(78,41,65),(79,41,62),(80,45,64),(81,45,60),(82,45,63);
 /*!40000 ALTER TABLE `likes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -179,10 +177,11 @@ CREATE TABLE `users` (
   `profile_picture` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL DEFAULT 'https://i.imgur.com/w367Hjs.jpeg',
   `admin` tinyint NOT NULL DEFAULT '0',
   `profile_picture_delete_hash` varchar(100) COLLATE utf8mb3_bin DEFAULT NULL,
+  `banned` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_id_UNIQUE` (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -191,7 +190,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (40,'Naida','Panjeta','naidapanjeta@gmail.com','17c00213ec89867b3598b4f972da329f','24','https://i.imgur.com/CeKesqG.png',0,'RDDdSpKJwAjrc78'),(41,'Benjamin','Peljto','benjaminpeljto00@gmail.com','00b19bfe043e029582cfa844b105654a','22','https://i.imgur.com/2NpFMoi.jpeg',0,'hUAQKPeJplyZBp6'),(42,'Anes','Pleh','znaciboze99@hotmail.com','00b19bfe043e029582cfa844b105654a','44','https://i.imgur.com/dMaO7OG.jpeg',0,'SpSw7YqH8Nw4A4s'),(43,'Benjamin','Peljto','admin@email.com','b549bd8404dd2ddf379742f88e65279f','23','https://i.imgur.com/w367Hjs.jpeg',1,NULL);
+INSERT INTO `users` VALUES (40,'Tarik','Panjeta','tarikpanjeta@gmail.com','00b19bfe043e029582cfa844b105654a','24','https://i.imgur.com/CeKesqG.png',0,'RDDdSpKJwAjrc78',0),(41,'Benjamin','Peljto','benjaminpeljto00@gmail.com','00b19bfe043e029582cfa844b105654a','22','https://i.imgur.com/eW5UC4N.png',0,'PQsEOzWJUBeRSEE',0),(42,'Neko','Ozbiljan','znaciboze99@hotmail.com','00b19bfe043e029582cfa844b105654a','44','https://i.imgur.com/dMaO7OG.jpeg',0,'SpSw7YqH8Nw4A4s',0),(43,'Benjamin','Peljto','admin@email.com','b549bd8404dd2ddf379742f88e65279f','23','https://i.imgur.com/w367Hjs.jpeg',1,NULL,0),(45,'Testni','Korisnik','benjaminpeljto00@hotmail.com','00b19bfe043e029582cfa844b105654a','25','https://i.imgur.com/YTRLzUL.png',0,'snZtEwOXQRxvZlk',0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -208,4 +207,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-01 13:34:08
+-- Dump completed on 2024-06-03 20:04:08
