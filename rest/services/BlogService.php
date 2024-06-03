1aa49288d813d55321f4904fbfe593db214691de
benjamin.peljto <benjaminpeljto00@gmail.com>
2024-06-03 19:09:17 +0200

    Modified BlogService.php Added 5 new methods for blogs filtering and one for getting specific user blogs.

--- rest/services/BlogService.php
+++ rest/services/BlogService.php
@@ -15,8 +15,26 @@ class BlogService extends BaseService
         return $this->dao->get_blogs_with_user();
     }
 
-    public function get_blogs_with_user_for_user($user_id){
-        return $this->dao->get_blogs_with_user_for_user($user_id);
+    public function get_blogs_all_random_with_user_for_user($user_id){
+        $blogs_newest = $this->dao->get_blogs_newest_with_user_for_user($user_id);
+        shuffle($blogs_newest);
+        return $blogs_newest;
+    }
+
+    public function get_blogs_newest_with_user_for_user($user_id){
+        return $this->dao->get_blogs_newest_with_user_for_user($user_id);
+    }
+
+    public function get_blogs_oldest_with_user_for_user($user_id){
+        return $this->dao->get_blogs_oldest_with_user_for_user($user_id);
+    }
+
+    public function get_blogs_most_liked_with_user_for_user($user_id){
+        return $this->dao->get_blogs_most_liked_with_user_for_user($user_id);
+    }
+
+    public function get_blogs_least_liked_with_user_for_user($user_id){
+        return $this->dao->get_blogs_least_liked_with_user_for_user($user_id);
     }
 
     public function get_blog_with_user_by_id($id){
@@ -25,5 +43,9 @@ class BlogService extends BaseService
 
     public function get_number_of_blogs(){
         return $this->dao->get_number_of_blogs();
+    }
+
+    public function get_my_blogs($user_id){
+        return $this->dao->get_my_blogs($user_id);
     }
 }
