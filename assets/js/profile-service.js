54bcb9b8d3521e081c2dd9e31b0b0af0e8aee17c
benjamin.peljto <benjaminpeljto00@gmail.com>
2024-06-03 19:16:47 +0200

    Modified profile-service.js Added necessary logic for profile editing and password change.

--- assets/js/profile-service.js
+++ assets/js/profile-service.js
@@ -2,6 +2,8 @@ var ProfileService = {
     init: function (){
         ProfileService.getProfileData();
         ProfileService.changeProfileImage();
+        ProfileService.editProfileDataSubmitter();
+        ProfileService.changeProfilePasswordSubmitter();
     },
 
     getProfileData: function (){
@@ -67,7 +69,145 @@ var ProfileService = {
                 toastr.error(error);
             }
         });
+    },
+
+    openEditProfileDataModal: function(){
+        $("#editProfileDataModal").modal("show");
+        ProfileService.populateEditProfileDataModalFields();
+    },
+
+    closeEditProfileDataModal: function(){
+        $("#editProfileDataModal").modal("hide");
+        ProfileService.resetEditProfileDataForm();
+    },
+
+    resetEditProfileDataForm: function(){
+        $("#editProfileDataForm").trigger("reset");
+    },
+
+    resetChangeProfilePasswordForm: function(){
+        $("#changeProfilePasswordForm").trigger("reset");
+    },
+
+    openChangeProfilePasswordModal: function(){
+        $("#changeProfilePasswordModal").modal("show");
+    },
+
+    closeChangeProfilePasswordModal: function(){
+        $("#changeProfilePasswordModal").modal("hide");
+        ProfileService.resetChangeProfilePasswordForm();
+    },
+
+    populateEditProfileDataModalFields: function(){
+        $("#editProfileFirstName").val($("#profile-first-name").html());
+        $("#editProfileLastName").val($("#profile-last-name").html());
+        $("#editProfileAge").val($("#profile-age").html());
+        $("#editProfileEmail").val($("#profile-email").html());
+    },
+
+    editProfileDataSubmitter: function(){
+        $("#editProfileDataForm").validate({
+            rules: {
+                first_name: "required",
+                last_name: "required",
+                age: {
+                    required: true,
+                    range: [15, 99],
+                    digits: true
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                password: {
+                    required: true,
+                }
+            },
+            messages: {
+                first_name: "This field cannot be empty.",
+                last_name: "This field cannot be empty.",
+                age: {
+                    required: "This field cannot be empty.",
+                    range: "Please enter an age between 15 and 99.",
+                    digits: "Please enter a valid age."
+                },
+                email: "Please enter a valid email address.",
+                password: {
+                    required: "Please confirm with your password.",
+                }
+            },
+            submitHandler: function(form, event) {
+                event.preventDefault();
+                ProfileService.updateProfileData(Utils.form2json(form));
+            }
+        });
+    },
+
+    updateProfileData(formJson){
+        RestClient.put(
+            "rest/profile/" + Utils.getCurrentUserId(),
+            formJson,
+            function(response){
+                    if(response.success){
+                        toastr.success(response.message);
+                        ProfileService.getProfileData();
+                        ProfileService.closeEditProfileDataModal();
+                    }
+                    else{
+                        toastr.error(response.message);
+                    }
+                }
+            )
+    },
+
+    changeProfilePasswordSubmitter: function(){
+        $("#changeProfilePasswordForm").validate({
+            rules: {
+                profileCurrentPassword: {
+                    required: true
+                },
+                password: {
+                    required: true,
+                    minlength: 8,
+                    maxlength: 16
+                },
+                password2: {
+                    required: true,
+                    equalTo: "#profileNewPassword"
+                }
+            },
+            messages: {
+                profileCurrentPassword: {
+                    required: "Please enter your current password."
+                },
+                password: {
+                    required: "Please enter a new password.",
+                    minlength: "Your password must be at least 8 characters long.",
+                    maxlength: "Your password cannot be longer than 16 characters."
+                },
+                password2: {
+                    required: "Please confirm your new password.",
+                    equalTo: "Passwords don't match."
+                }
+            },
+            submitHandler: function(form, event) {
+                event.preventDefault();
+                ProfileService.changePassword(Utils.form2json(form));
+            }
+        });
+    },
+
+    changePassword: function(data){
+        RestClient.put("rest/profile/change-password/" + Utils.getCurrentUserId(),
+            data,
+            function(response){
+                    if(response.success){
+                        toastr.success(response.message);
+                        ProfileService.closeChangeProfilePasswordModal();
+                    }
+                    else{
+                        toastr.error(response.message);
+                    }
+            });
     }
-
-
 }
